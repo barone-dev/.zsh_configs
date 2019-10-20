040d762f104fdab6503d8aa9e8b0d967891a8427
barone-dev <marco.barone.belo@gmail.com>
2019-10-20 11:28:17 -0300

    changed repo name

--- aliases/manager.sh
+++ aliases/manager.sh
@@ -1,9 +1,10 @@
-source ~/zsh_configs/aliases/git.sh
-source ~/zsh_configs/aliases/python.sh
-source ~/zsh_configs/aliases/yay.sh
-source ~/zsh_configs/aliases/zsh.sh
+source ~/.zsh_configs/aliases/git.sh
+source ~/.zsh_configs/aliases/python.sh
+source ~/.zsh_configs/aliases/yay.sh
+source ~/.zsh_configs/aliases/zsh.sh
 
 # Load my current job secrets aliases/infos:
-for file in ~/zsh_configs/aliases/secret/*; do
+for file in ~/.zsh_configs/aliases/secret/*; 
+do
     source $file
 done
