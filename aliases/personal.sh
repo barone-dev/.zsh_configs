0fca24cd9ae1b08ec0b809f612c1a673cd7b2f89
mrsbelo <mrsmbelo@gmai.com>
2020-03-15 09:57:04 -0300

    adding my git user name email auto config

--- aliases/personal.sh
+++ aliases/personal.sh
@@ -1,2 +1,3 @@
 alias pro_python_configs="cd ~/Projects/python_configs && code . && . venv/bin/activate"
 alias pro_django_portfolio_rp="cd ~/Projects/django_portfolio_rp && code . && . venv/bin/activate"
+alias personal_git='git config --global user.name "marcobelo" && git config --global user.email "marco.barone.belo@gmai.com"'
