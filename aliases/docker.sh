7a7e65f1c5cbdd4008cbf8e720df548be6b7025f
marcobelo <marco.barone.belo@gmail.com>
2020-03-19 08:52:01 -0300

    edited git users configs

--- aliases/docker.sh
+++ aliases/docker.sh
@@ -1,5 +1,5 @@
-alias d_system_clean_up="docker system prune -a"
-alias d_system_space_usage="docker system df"
+# alias d_system_clean_up="docker system prune -a"
+# alias d_system_space_usage="docker system df"
 
-alias d_images_ids="docker images -a -q"
-alias d_images_delete="docker rmi -f $(docker images -a -q)"
+# alias d_images_ids="docker images -a -q"
+# alias d_images_delete="docker rmi -f $(docker images -a -q)"
--- aliases/personal.sh
+++ aliases/personal.sh
@@ -1,3 +1,5 @@
 alias pro_python_configs="cd ~/Projects/python_configs && code . && . venv/bin/activate"
 alias pro_django_portfolio_rp="cd ~/Projects/django_portfolio_rp && code . && . venv/bin/activate"
-alias personal_git='git config --global user.name "marcobelo" && git config --global user.email "marco.barone.belo@gmail.com"'
+alias study_git='git config --global user.name "marcobelo" && git config --global user.email "marco.barone.belo@gmail.com"  && echo "Git user: $(git config user.name)" && echo "Git email: $(git config user.email)"'
+alias study='study_git && cd ~/Documents/study/'
+alias work='work_git && cd ~/Documents/work/'
