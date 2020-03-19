1a21cdad5a88a21dae571a09aa9e80e623deeaab
marcobelo <marco.barone.belo@gmail.com>
2020-03-19 09:08:55 -0300

    added vpn_access command

--- aliases/personal.sh
+++ aliases/personal.sh
@@ -3,3 +3,4 @@ alias pro_django_portfolio_rp="cd ~/Projects/django_portfolio_rp && code . && . 
 alias study_git='git config --global user.name "marcobelo" && git config --global user.email "marco.barone.belo@gmail.com"  && echo "Git user: $(git config user.name)" && echo "Git email: $(git config user.email)"'
 alias study='study_git && cd ~/Documents/study/'
 alias work='work_git && cd ~/Documents/work/'
+alias vpn_access="cd ~ && sudo openvpn --config vpn.ovpn --auth-user-pass vpn_auth.txt"
