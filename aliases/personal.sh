5304b2e117d15f466e5baf17086279df419d7792
marcobelo <marco.barone.belo@gmail.com>
2020-03-19 08:42:08 -0300

    fix email in personal_git

--- aliases/personal.sh
+++ aliases/personal.sh
@@ -1,3 +1,3 @@
 alias pro_python_configs="cd ~/Projects/python_configs && code . && . venv/bin/activate"
 alias pro_django_portfolio_rp="cd ~/Projects/django_portfolio_rp && code . && . venv/bin/activate"
-alias personal_git='git config --global user.name "marcobelo" && git config --global user.email "marco.barone.belo@gmai.com"'
+alias personal_git='git config --global user.name "marcobelo" && git config --global user.email "marco.barone.belo@gmail.com"'
