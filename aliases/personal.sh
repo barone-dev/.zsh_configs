42efa839f7c35207580775822c88a3ef7feeca6e
marcobelo <marco.barone.belo@gmail.com>
2020-05-04 10:11:54 -0300

    edited work alias

--- aliases/personal.sh
+++ aliases/personal.sh
@@ -2,5 +2,5 @@ alias pro_python_configs="cd ~/Projects/python_configs && code . && . venv/bin/a
 alias pro_django_portfolio_rp="cd ~/Projects/django_portfolio_rp && code . && . venv/bin/activate"
 alias study_git='git config --global user.name "marcobelo" && git config --global user.email "marco.barone.belo@gmail.com"  && echo "Git user: $(git config user.name)" && echo "Git email: $(git config user.email)"'
 alias study='study_git && cd ~/Documents/study/'
-alias work='work_git && cd ~/Documents/work/'
+alias work='work_git && cd ~/Documents/work/all_repos/'
 alias vpn_access="cd ~ && sudo openvpn --config vpn.ovpn --auth-user-pass vpn_auth.txt"
